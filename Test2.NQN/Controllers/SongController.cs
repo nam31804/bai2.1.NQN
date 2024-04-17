882835c318474e97f3616b645f9f42f0b8fe0f96
nam31804 <nam31804@gmail.com>
2024-04-17 23:16:30 +0700

    Add project files.

---
+++ Test2.NQN/Controllers/SongController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test2.NQN.Models;
+using static Test2.NQN.Models.SongController;
+
+namespace Test2.NQN.Controllers
+{
+    public class SongController : Controller
+    {
+        // GET: Song
+        public ActionResult Index()
+        {
+            var songs = new List<Song>
+        {
+            new Song { Title = "Song 1", Artist = "Artist 1", FilePath = "/path/to/song1.mp3" },
+            new Song { Title = "Song 2", Artist = "Artist 2", FilePath = "/path/to/song2.mp3" },
+            new Song { Title = "Song 3", Artist = "Artist 3", FilePath = "/path/to/song3.mp3" }
+        };
+
+            return View(songs);
+        }
+
+        public FileResult Download(string filePath)
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(Server.MapPath(filePath));
+            string fileName = Path.GetFileName(filePath);
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+    }
+}
---
+++ Test2.NQN/Models/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test2.NQN.Models
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string FilePath { get; set; }
+    }
+}
